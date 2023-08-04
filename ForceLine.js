58b31b9fb270c51da446c0d39428d76f948969bc
xaddict <darklightdusk@gmail.com>
2023-08-05 00:25:21 +0200

    Add noise for color

--- ForceLine.js
+++ ForceLine.js
@@ -1,3 +1,5 @@
+import { createNoise3D } from 'simplex-noise';
+
 function linear(x, fromStart, fromEnd, toStart, toEnd) {
   return (
     ((x - fromStart) / (fromEnd - fromStart)) * (toEnd - toStart) + toStart
@@ -22,13 +24,13 @@ export default class ForceLine {
     const linearForce1 = linear(this.force[1], -1, 1, 0, 1);
     const endX = this.x + this.force[0] * 100;
     const endY = this.y + this.force[1] * 100;
-    const hue = linearForce0 * 360;
-    const saturation = linearForce1 * 100;
     const lineWidth =
       Math.sqrt(Math.pow(this.force[0], 2) + Math.pow(this.force[1], 2)) * 10;
 
     paper.beginPath();
     const gradient = paper.createLinearGradient(this.x, this.y, endX, endY);
+    const noise = createNoise3D(this.x, this.y, this.z);
+    const hue = linear(noise, -1, 1, 0, 360);
     gradient.addColorStop(0, `hsl(${hue}, 100%, 10%)`);
     gradient.addColorStop(1, `hsl(${hue}, 100%, 50%)`);
     paper.strokeStyle = gradient;
